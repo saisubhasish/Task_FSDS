2b34b6f1f9fc6f8d17891f5e3a5efcc890dabdc8
saisubhasish <102937478+saisubhasish@users.noreply.github.com>
2022-08-23 19:47:38 +0530

    Update TaskGETsqlTable.py

--- TaskGETsqlTable.py
+++ TaskGETsqlTable.py
@@ -20,12 +20,12 @@ def get_data_from():
     db = request.args.get('db')
     tn = request.args.get('tn')
     try:
-        con = conn.connect(host="localhost", user="root", password="Root_mysql", database=db)
-        cur = con.cursor(dictionary=True)
+        connection = conn.connect(host="localhost", user="root", password="Root_mysql", database=db)
+        cursor = connection.cursor(dictionary=True)
         cur.execute(f'select * from {tn}')
-        data = cur.fetchall()
-        con.commit()
-        con.close()
+        data = cursor.fetchall()
+        connection.commit()
+        connection.close()
     except Exception as e:
         return jsonify(str(e))
     return jsonify(data)
