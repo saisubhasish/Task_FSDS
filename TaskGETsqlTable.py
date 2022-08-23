5f6c1efac6d5774aeb69344ca6af8208d50e4ef0
saisubhasish <102937478+saisubhasish@users.noreply.github.com>
2022-08-23 19:35:35 +0530

    Add files via upload

---
+++ TaskGETsqlTable.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, jsonify
+import mysql.connector as conn
+
+app = Flask(__name__)
+
+"""
+    This file contains GET request to pass through url and print it in the webpage
+    To take the table name from url and print all the data in webpage
+"""
+
+@app.route("/testfun")
+def test():
+    get_name = request.args.get("get_name")
+    mobile_number = request.args.get("mobile")
+    mail_id = request.args.get("mail_id")
+    return "this is my function for get {} {} {}".format(get_name, mobile_number, mail_id)
+
+@app.route('/get_data')
+def get_data_from():
+    db = request.args.get('db')
+    tn = request.args.get('tn')
+    try:
+        con = conn.connect(host="localhost", user="root", password="Root_mysql", database=db)
+        cur = con.cursor(dictionary=True)
+        cur.execute(f'select * from {tn}')
+        data = cur.fetchall()
+        con.commit()
+        con.close()
+    except Exception as e:
+        return jsonify(str(e))
+    return jsonify(data)
+
+
+
+if __name__ == "__main__":
+    app.run(port=5002)
